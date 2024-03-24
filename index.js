5759f8849ea8be7cebb21f1e2f77c47a4d1c8475
ArnoldMajor <arnoldmwangakala@gmail.com>
2024-03-24 19:03:50 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+const employee = {};
+
+employee.name = 'Arnold';
+employee.streetAddress = 'Sinza';
+
+console.log (employee);
+
+function updateEmployeeWithKeyAndValue (employee, key, value){
+    let newEmployee;
+    newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+console.log (updateEmployeeWithKeyAndValue(employee, 'Age', '30'));
+
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+    employee.name = 'Sam';
+    employee.streetAddress = '12 Broadway'
+    return employee;
+}
+
+console.log (destructivelyUpdateEmployeeWithKeyAndValue());
+
+function deleteFromEmployeeByKey (employee, key){
+    let newEmployee
+    newEmployee = {...employee};
+    delete newEmployee[key]
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
